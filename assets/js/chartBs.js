f80093c0424a64ed637e8d92d4ac8878765b0851
Oleksandr Papevis <sasha@papevis.com>
2021-01-26 15:43:29 +0200

    feat: progress

--- assets/js/chartBs.js
+++ assets/js/chartBs.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
-  var amp = 0.1;        // active integrator gain
-  var linearAmp =22.2;  // inverting amplifier gain
-  var shift = 16777216; // active integrator accumulating division factor
-                  // Initial value for normalizedA
-                  // |     |
-  var accumulatorA = 1000.0 * shift / amp;
-                  // \_____/
+  var amp = 0.1;        /* active integrator gain */
+  var linearAmp =22.2;  /* inverting amplifier gain */
+  var shift = 16777216; /* active integrator accumulating division factor */
+                  /* Initial value for normalizedA */
+                  /* |     | */
+  var accumulatorA = -1000.0 * shift;
+                  /* \_____/ */
   var accumulatorB = 0.0, normalizedA = 0.0,
       nextValue = 0.0,    normalizedB = 0.0;
   var labels = [], normsA = [],
       values = [], normsB = [];
 
   for(i=1; i<50000; i++){
-    // nextValue incomes as a closed-loop to R1 from DA3
+    /* nextValue incomes as a closed-loop to R1 from DA3 */
     var thisValue = -nextValue * linearAmp
     accumulatorA += thisValue;
     thisValue = accumulatorA / shift;
@@ -30,26 +30,26 @@ document.addEventListener('DOMContentLoaded', function(){
       normsA.push(normalizedA);
       normsB.push(normalizedB);
     }
-    // time constant of integrators is changed at the following time steps
-    if(i==12960){ shift /= 4; accumulatorA /= 4; accumulatorB /= 4; }
-    if(i==23768){ shift *= 4; accumulatorA *= 4; accumulatorB *= 4; }
-    if(i==29000){ shift *= 2; accumulatorA *= 2; accumulatorB *= 2; }
+    /* time constant of integrators is changed at the following time steps */
+    if(i==8640){ shift /= 4; accumulatorA /= 4; accumulatorB /= 4; }
+    /* if(i==23768){ shift *= 4; accumulatorA *= 4; accumulatorB *= 4; } */
+    /* if(i==29000){ shift *= 2; accumulatorA *= 2; accumulatorB *= 2; } */
   }
-  var chartData = {//from w  ww.j a v  a 2 s.com
+  var chartData = {/* from w  ww.j a v  a 2 s.com */
     CC: [{
       code: 'oscilator',
       labels: labels,
-      datasets: [/*{
+      datasets: [{
         label: 'value',
-        backgroundColor: "rgba(255,0,0,1)",
+        backgroundColor: "rgba(255,0,0,0.5)",
         data: values
-      },*/{
+      },{
         label: 'normalizedA',
-        backgroundColor: "rgba(0,127,0,1)",
+        backgroundColor: "rgba(0,127,0,0.5)",
         data: normsA
       },{
         label: 'normalizedB',
-        backgroundColor: "rgba(0,0,127,1)",
+        backgroundColor: "rgba(0,0,127,0.5)",
         data: normsB
       }]
     }]
@@ -60,6 +60,16 @@ document.addEventListener('DOMContentLoaded', function(){
     canvas.id = chartId;
     document.querySelector('.content .charts').appendChild(canvas);
     var context = document.getElementById(chartId).getContext('2d');
+    var scales = JSON.parse(JSON.stringify(data.datasets));
+    scales.map((data) => {
+      data["stacked"] = true;
+      data["id"] = data["label"];
+      delete data["label"];
+    });
+    data.datasets.map((data) => {
+      data["yAxisID"] = data["label"];
+    });
+    console.log('scales', scales);
     window[chartId] = new Chart(context, {
       type: 'bar',
       data: data,
@@ -77,9 +87,7 @@ document.addEventListener('DOMContentLoaded', function(){
           xAxes: [{
             stacked: true,
           }],
-          yAxes: [{
-            stacked: true
-          }]
+          yAxes: scales
         }
       }
     });
